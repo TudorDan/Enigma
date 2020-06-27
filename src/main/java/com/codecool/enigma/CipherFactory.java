76f1a81c35a66478d31c897e04864bac2bc9db62
TudorDan <tudor.ist@gmail.com>
2020-06-27 22:49:13 +0300

    Refactor Cipher factory

--- src/main/java/com/codecool/enigma/CipherFactory.java
+++ src/main/java/com/codecool/enigma/CipherFactory.java
@@ -1,12 +1,29 @@
 package com.codecool.enigma;
 
-//class CipherFactory {
-//
-//    static boolean isCipherAvailable(String cipherName) {
-//        return false;
-//    }
-//
-//    static Cipher getCipherForArgs(ArgsParser args) throws EnigmaException {
-//        return null;
-//    }
-//}
+class CipherFactory {
+
+    static boolean isCipherAvailable(String cipherName) {
+        return cipherName != null && (cipherName.equals("rot13") || cipherName.equals("rail-fence") || cipherName.equals(
+                "morse"));
+    }
+
+    static Cipher getCipherForArgs(ArgsParser args) throws EnigmaException {
+        switch (args.cipher) {
+            case "rot13":
+                return new Rot13();
+            case "morse":
+                return new MorseCode();
+            case "rail-fence":
+                if (args.key == null) {
+                    throw new EnigmaException("Key is missing!");
+                }
+                try {
+                    int key = Integer.parseInt(args.key);
+                    return new RailFence(key);
+                } catch (Exception e) {
+                    throw new EnigmaException("Key must be an integer!");
+                }
+        }
+        return null;
+    }
+}
